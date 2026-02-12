9275d4d46d309d999beaa31c024bb50a9e9dffc9
allocguy <srinidhi@alloclabs.com>
2026-02-12 10:37:01 +0000

    Mirror CLI from platform 8f41912

--- src/alloc/__init__.py
+++ src/alloc/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from alloc.ghost import ghost, GhostReport
 from alloc.callbacks import AllocCallback as HuggingFaceCallback
